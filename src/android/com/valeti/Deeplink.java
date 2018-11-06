e57b4c1d93d239dc449ef4fdbb0696e3da0f630b
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2018-11-06 17:19:45 -0200

    go

--- src/android/com/valeti/Deeplink.java
+++ src/android/com/valeti/Deeplink.java
@@ -3,8 +3,6 @@ package org.apache.cordova.test;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
-import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -15,17 +13,10 @@ import android.util.Base64;
 * This class exposes methods in Cordova that can be called from JavaScript.
 */
 public class Deeplink extends CordovaPlugin {
-    
+
     public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("echo")) {
-            // callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, args.getString(0)));
             callbackContext.success(args.getString(0));
-        } else if(action.equals("echoAsync")) {
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    // callbackContext.sendPluginResult( new PluginResult(PluginResult.Status.OK, args.optString(0)));
-                }
-            });
         } else {
             callbackContext.error("error");
             return false;
