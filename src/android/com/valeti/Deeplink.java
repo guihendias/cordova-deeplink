33c284291a9dbb0f48dc8845de08032404593659
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2018-11-06 16:59:37 -0200

    go

--- src/android/com/valeti/Deeplink.java
+++ src/android/com/valeti/Deeplink.java
@@ -4,6 +4,7 @@ package org.apache.cordova.test;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -16,16 +17,10 @@ import android.util.Base64;
 public class Deeplink extends CordovaPlugin {
 
 
-     private volatile boolean bulkEchoing;
+     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+         super.initialize(cordova, webView);
+     }
 
-     /**
-     * Executes the request and returns PluginResult.
-     *
-     * @param action            The action to execute.
-     * @param args              JSONArry of arguments for the plugin.
-     * @param callbackContext   The callback context from which we were invoked.
-     */
-    @SuppressLint("NewApi")
     public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("echo")) {
             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, args.getString(0)));
