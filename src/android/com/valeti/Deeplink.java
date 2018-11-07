59014b049131be0a5c7b97c9f52fcbbcc22e16d2
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2018-11-07 11:56:55 -0200

    go

--- src/android/com/valeti/Deeplink.java
+++ src/android/com/valeti/Deeplink.java
@@ -32,7 +32,7 @@ public class Deeplink extends CordovaPlugin {
             bundle.putString("currencyPosition", "CURRENCY_AFTER_AMOUNT");
             bundle.putString("currencyCode", "986");
 
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("getnet://pagamento/v1/payment"));
+            Intent intent = new Intent(this, Uri.parse("getnet://pagamento/v1/payment"));
 
             intent.putExtras(bundle);
             cordova.startActivityForResult((CordovaPlugin) this, intent, REQUEST_CODE);
