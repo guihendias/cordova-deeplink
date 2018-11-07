09ea2e28d1273a5c20d93fa9a3f9a58b8a74a0e4
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2018-11-07 15:50:40 -0200

    go

--- www/plugin.js
+++ www/plugin.js
@@ -5,7 +5,11 @@ var exec = cordova.require("cordova/exec");
 var PLUGIN_NAME = 'Deeplink';
 
 var Deeplink = {
-    payment: (phrase, cb) => exec(cb, (data) => alert(data), PLUGIN_NAME, "payment", [phrase])
+    payment: (phrase, cb) => exec(cb, (data) => alert(data), PLUGIN_NAME, "payment", [phrase]),
+    preAuthorization: (phrase, cb) => exec(cb, (data) => alert(data), PLUGIN_NAME, "pre-authorization", [phrase]),
+    refund: (phrase, cb) => exec(cb, (data) => alert(data), PLUGIN_NAME, "refund", [phrase]),
+    reprint: (phrase, cb) => exec(cb, (data) => alert(data), PLUGIN_NAME, "reprint", [phrase]),
+    getInfos: (phrase, cb) => exec(cb, (data) => alert(data), PLUGIN_NAME, "getinfos", [phrase])
 };
 
 module.exports = Deeplink;
