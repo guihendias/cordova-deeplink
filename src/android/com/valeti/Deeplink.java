346aca2473ea94ed70d54e4a997618da566d36e1
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2018-11-07 10:23:01 -0200

    go

--- src/android/com/valeti/Deeplink.java
+++ src/android/com/valeti/Deeplink.java
@@ -1,23 +1,74 @@
 package com.valeti;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+import android.content.Intent;
+import android.os.Bundle;
 
 /**
  * This class exposes methods in Cordova that can be called from JavaScript.
  */
 public class Deeplink extends CordovaPlugin {
 
+    private CallbackContext PUBLIC_CALLBACKS = null;
+    private final int REQUEST_CODE = 1001;
+    private final String ARG_RESULT = "result";
+    private final String ARG_RESULT_DETAILS = "resultDetails";
+    private final String ARG_AMOUNT = "amount";
+    private final String ARG_TYPE = "type";
+    private final String ARG_INPUT_TYPE = "inputType";
+    private final String ARG_INSTALLMENTS = "installments";
+    private final String ARG_NSU = "nsu";
+    private final String ARG_BRAND = "brand";
+
     public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext)
             throws JSONException {
-        if (action.equals("echo")) {
-            callbackContext.success(args.getString(0));
-        } else {
-            callbackContext.error("error");
-            return false;
+        if (action.equals("payment")) {
+            Bundle bundle = new Bundle();
+            bundle.putString("amount", "000000001000");
+            bundle.putString("currencyPosition", "CURRENCY_AFTER_AMOUNT");
+            bundle.putString("currencyCode", "986");
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("getnet://pagamento/v1/payment"));
+            intent.putExtras(bundle);
+            cordova.startActivityForResult((CordovaPlugin) this, intent, REQUEST_CODE);
+            // callbackContext.success(args.getString(0));
         }
+
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+        pluginResult.setKeepCallback(true);
+
         return true;
+    }
+
+    @Override
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == cordova.getActivity().RESULT_OK) {
+            Bundle extras = data.getExtras();// Get data sent by the Intent
+            String result = extras.getString(ARG_RESULT); // data parameter will be send from the other activity.
+            String resultDetails = extras.getString(ARG_RESULT_DETAILS);
+            String amount = data.getString(ARG_AMOUNT);
+            String type = data.getString(ARG_TYPE);
+            String inputType = data.getString(ARG_INPUT_TYPE);
+            String installments = data.getString(ARG_INSTALLMENTS);
+            String nsu = data.getString(ARG_NSU);
+            String brand = data.getString(ARG_BRAND);
+
+            PluginResult resultado = new PluginResult(PluginResult.Status.OK, "this value will be sent to cordova");
+            resultado.setKeepCallback(true);
+            PUBLIC_CALLBACKS.sendPluginResult(resultado);
+
+            return;
+        } else if (resultCode == cordova.getActivity().RESULT_CANCELED) {
+            PluginResult resultado = new PluginResult(PluginResult.Status.OK,
+                    "canceled action, process this in javascript");
+            resultado.setKeepCallback(true);
+            PUBLIC_CALLBACKS.sendPluginResult(resultado);
+            return;
+        }
+        // Handle other results if exists.
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- www/plugin.js
+++ www/plugin.js
@@ -2,13 +2,10 @@
 /* globals window */
 
 var exec = cordova.require("cordova/exec");
-var PLUGIN_NAME = "Deeplink";
+var PLUGIN_NAME = 'Deeplink';
 
 var Deeplink = {
-    echo: (phrase, cb) =>
-        exec(data => alert(data), data => alert(data), PLUGIN_NAME, "echo", [
-            phrase
-        ])
+    payment: (phrase, cb) => exec(cb, null, PLUGIN_NAME, "payment", [phrase])
 };
 
 module.exports = Deeplink;
